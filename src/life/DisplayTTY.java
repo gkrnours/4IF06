e441562034ab43e56c88cd10d775f086c3645966
Coues Ludovic <couesl@gmail.com>
2013-02-21 21:12:36 +0100

    An experimental display class

---
+++ src/life/DisplayTTY.java
@@ -0,0 +1,32 @@
+package life;
+
+import java.util.Iterator;
+
+public class DisplayTTY extends Display{
+	
+	public DisplayTTY(LIFE life) {
+		super(life);
+	}
+	
+	public static String[] utf8 = {".","#", "▢","▣", "░","▓", "◇","◈", "☐","☒"};
+	public void show(){
+		update();
+	}
+	public void update(){
+		int style = 0;
+		Iterator<Coord> raw = life.raw();
+		Coord next = (raw.hasNext())?raw.next():null;
+		
+		for(int y=life.y(), yp=y+life.h(); y<yp; ++y){
+			for(int x=life.x(), xp=x+life.w(); x<xp; ++x){
+				if(next!=null && next.isAt(x,y)){
+					System.out.print(utf8[1+style]);
+					next = (raw.hasNext())?raw.next():null;
+				} else {
+					System.out.print(utf8[0+style]);
+				}
+			}
+			System.out.println();
+		}
+	}
+}
--- src/life/DisplayVT100.java
+++ src/life/DisplayVT100.java
@@ -2,28 +2,39 @@ package life;
 
 import java.util.Iterator;
 
+import net.slashie.libjcsi.CSIColor;
+import net.slashie.libjcsi.ConsoleSystemInterface;
+import net.slashie.libjcsi.wswing.WSwingConsoleInterface;
+
 public class DisplayVT100 extends Display{
-	
+	private ConsoleSystemInterface csi;
 	public DisplayVT100(LIFE life) {
 		super(life);
 	}
 	
 	public static String[] utf8 = {".","#", "▢","▣", "░","▓", "◇","◈", "☐","☒"};
 	public void show(){
+		csi = new WSwingConsoleInterface("LIFE");
+		csi.cls();
+		update();
+	}
+	
+	public void update(){
 		int style = 0;
 		Iterator<Coord> raw = life.raw();
 		Coord next = (raw.hasNext())?raw.next():null;
+		csi.cls();
 		
-		for(int y=life.y(), yp=y+life.h(); y<yp; ++y){
-			for(int x=life.x(), xp=x+life.w(); x<xp; ++x){
+		for(int y=life.y(), yp=y+life.h(), dy=-y; y<yp; ++y){
+			for(int x=life.x(), xp=x+life.w(), dx = -x; x<xp; ++x){
 				if(next!=null && next.isAt(x,y)){
-					System.out.print(utf8[1+style]);
+					csi.print(x+dx, y+dy, "#", CSIColor.CARMINE);
 					next = (raw.hasNext())?raw.next():null;
 				} else {
-					System.out.print(utf8[0+style]);
+					csi.print(x+dx, y+dy, ".", CSIColor.CELADON);
 				}
 			}
-			System.out.println();
 		}
+		csi.refresh();
 	}
 }
